ef00ac27013c15a6a1505198c9c71e2522c48db2
TechAniket123 <kumaraniketpathak123@gmail.com>
2023-06-02 04:57:06 +0530

    Created model Chapter

--- BoardSelection/models.py
+++ BoardSelection/models.py
@@ -10,3 +10,13 @@ class Topic(models.Model):
     topic_name = models.CharField(max_length=40)
     def __str__(self):
         return self.chapter_name+' - '+ self.topic_name
+
+class Chapter(models.Model):
+    order_number = models.IntegerField()
+    board_name = models.CharField(max_length=40)
+    subject_name = models.CharField(max_length=40)
+    class_name = models.CharField(max_length=25)
+    chapter_name = models.CharField(max_length=40)
+    pdf_link = models.CharField(max_length=255)
+    def __str__(self):
+        return self.chapter_name+' - '+ self.subject_name
